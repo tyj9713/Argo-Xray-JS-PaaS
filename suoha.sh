b01d98c8ae62ff7a6f1b8d44e90dbc5fdcf4bfa8
tyj9713 <tyj9713@qq.com>
2025-04-20 18:08:24 +0800

    Update suoha.sh

--- suoha.sh
+++ suoha.sh
@@ -1,3 +1,43 @@
+#!/bin/bash
+# onekey suoha
+linux_os=("Debian" "Ubuntu" "CentOS" "Fedora" "Alpine")
+linux_update=("apt update" "apt update" "yum -y update" "yum -y update" "apk update")
+linux_install=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+n=0
+for i in `echo ${linux_os[@]}`
+do
+	if [ $i == $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') ]
+	then
+		break
+	else
+		n=$[$n+1]
+	fi
+done
+if [ $n == 5 ]
+then
+	echo 当前系统$(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2)没有适配
+	echo 默认使用APT包管理器
+	n=0
+fi
+if [ -z $(type -P unzip) ]
+then
+	${linux_update[$n]}
+	${linux_install[$n]} unzip
+fi
+if [ -z $(type -P curl) ]
+then
+	${linux_update[$n]}
+	${linux_install[$n]} curl
+fi
+if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') != "Alpine" ]
+then
+	if [ -z $(type -P systemctl) ]
+	then
+		${linux_update[$n]}
+		${linux_install[$n]} systemctl
+	fi
+fi
+
 function quicktunnel(){
 rm -rf xray cloudflared-linux xray.zip
 case "$(uname -m)" in
@@ -156,3 +196,27 @@ done
 
 cat v2ray.txt
 }
+
+# 设置默认参数
+	mode=1
+		protocol=2
+		ips=4
+
+# 清理历史进程
+	if [ $(grep -i PRETTY_NAME /etc/os-release | cut -d \" -f2 | awk '{print $1}') == "Alpine" ]
+	then
+		kill -9 $(ps -ef | grep xray | grep -v grep | awk '{print $1}') >/dev/null 2>&1
+		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $1}') >/dev/null 2>&1
+	else
+		kill -9 $(ps -ef | grep xray | grep -v grep | awk '{print $2}') >/dev/null 2>&1
+		kill -9 $(ps -ef | grep cloudflared-linux | grep -v grep | awk '{print $2}') >/dev/null 2>&1
+	fi
+
+# 清理历史文件
+	rm -rf xray cloudflared-linux v2ray.txt
+
+# 获取ISP信息
+isp=$(curl -$ips -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18"-"$30}' | sed -e 's/ /_/g')
+
+# 执行梭哈模式
+	quicktunnel
